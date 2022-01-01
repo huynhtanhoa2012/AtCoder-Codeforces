4f84766bcfda39b6d98b7e5ddae1b2ea7f718d4c
huynhtanhoa2012 <huynhtanhoa2013@gmail.com>
2022-01-02 02:38:10 +1100

    update

--- CP Course/Data Structure Recap/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-for(int i=0; i<arr.size(); i++){
-    //     if(arr[i] == x){
-    //         for(int j=0; j<arr.size(); j++){
-    //             s1[j].first = arr[i] - arr[j];
-    //             s1[j].second = arr[j];
-    //         }
-    //         sort(s1.begin(), s1.end());
-    //     }
-    // }
--- CodeForces Problem B/129B.cpp
+++ CodeForces Problem B/129B.cpp
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h>  
  
 using namespace std;
-const int inf = 1000000000; // 10^9
 typedef long long ll;
-
-
-int findKthLargest(vector<int>& nums, int k) {
-    sort(nums.begin(), nums.end(), greater<int>());
-    cout << nums[k-1];
-}
- 
+    
+    
 int main(){
  
 #ifndef ONLINE_JUDGE
 freopen("input.txt", "r", stdin);
 freopen("output.txt", "w", stdout);
 #endif
-    
     
     
     
